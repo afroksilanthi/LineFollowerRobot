471b3bbc13e70f97f74a8a476353948b5e6756eb
Billis Ch <26091373+BillisC@users.noreply.github.com>
2024-04-25 18:05:23 +0300

    Update algorithm to use PID for two sensors

--- code.py
+++ code.py
@@ -1,91 +1,117 @@
 import machine, time
 
-#Left Motor
-LMotorRedPin = machine.Pin(10, machine.Pin.OUT)#Forwards
-LMotorBlackPin = machine.Pin(11, machine.Pin.OUT)#Backwards
+def percentage(percent):
+    return int(65565 * (percent / 100))
 
-#speed L
-pwmLeftMotorR = machine.PWM(LMotorRedPin) #red, forwards
-pwmLeftMotorR.freq(50)
-pwmLeftMotorB = machine.PWM(LMotorBlackPin) #black, backwards
-pwmLeftMotorB.freq(50)
+# --- Parameters ---
+# DC Motors
+LMPins = { "RED" : 10, "BLACK" : 11 }
+LMRed = pwmLMRed = LMBlack = pwmLMBlack = None
+RMPins = { "RED" : 8,  "BLACK" : 9 }
+RMRed = pwmRMRed = RMBlack = pwmRMBlack = None
+MFreq = 200
 
+# IR Sensors
+IRPins = { "LEFT" : 17, "RIGHT" : 26 }
+RInfrared = LInfrared = None
 
-#Right Motor
-RMotorRedPin = machine.Pin(8, machine.Pin.OUT)#Front
-RMotorBlackPin = machine.Pin(9, machine.Pin.OUT)#Back
+# PID
+Kp = 6.5     # Curve smoothness
+Ki = 0.0001  # Reduce oscillation
+Kd = 0.6     # Tight curve
 
-#speed R
-pwmRightMotorR = machine.PWM(RMotorRedPin) #red, forwards
-pwmRightMotorR.freq(50)
-pwmRightMotorB = machine.PWM(RMotorBlackPin) #black, backwards
-pwmRightMotorB.freq(50)
+# --- Initialization ---
+def MotorsInit():
+    # Initialize motors and setup PWM to the desired frequency.
+    global LMRed, pwmLMRed, LMBlack, pwmLMBlack
+    global RMRed, pwmRMRed, RMBlack, pwmRMBlack
+    
+    # Left Motor (Red = Forward, Black = Backward)
+    LMRed = machine.Pin(LMPins["RED"], machine.Pin.OUT)
+    pwmLMRed = machine.PWM(LMRed, freq=MFreq, duty_u16=0)
+    LMBlack = machine.Pin(LMPins["BLACK"], machine.Pin.OUT)
+    pwmLMBlack = machine.PWM(LMBlack, freq=MFreq, duty_u16=0)
+    
+    # Right Motor (Red = Forward, Black = Backward)
+    RMRed = machine.Pin(RMPins["RED"], machine.Pin.OUT)
+    pwmRMRed = machine.PWM(RMRed, freq=MFreq, duty_u16=0)
+    RMBlack = machine.Pin(RMPins["BLACK"], machine.Pin.OUT)
+    pwmRMBlack = machine.PWM(RMBlack, freq=MFreq, duty_u16=0)
 
+def InfraredInit():
+    # Initialize all infrared sensor pins with default state set to 0.
+    global RInfrared, LInfrared
+    
+    # Right IR Sensor
+    RInfrared = machine.Pin(IRPins["RIGHT"], machine.Pin.IN, machine.Pin.PULL_DOWN)
+    # Left IR Sensor
+    LInfrared = machine.Pin(IRPins["LEFT"], machine.Pin.IN, machine.Pin.PULL_DOWN)
 
+# --- Movement ---
+# def RForward():
+#     pwmRMRed.duty_u16(MSpeed)
+#     pwmRMBlack.duty_u16(0)
+# 
+# def RStop():
+#     pwmRMRed.duty_u16(0)
+#     pwmRMBlack.duty_u16(0)
+# 
+# def LForward():
+#     pwmLMRed.duty_u16(MSpeed)
+#     pwmLMBlack.duty_u16(0)
+# 
+# def LStop():
+#     pwmLMRed.duty_u16(0)
+#     pwmLMBlack.duty_u16(0)
 
+def IRPosition():
+    # If black line is on the center then return 0
+    # If black line is on the right then return 1
+    # If black line is on the left then return -1
+    return (RInfrared.value() - LInfrared.value())
+
+# Run init functions
+MotorsInit()
+InfraredInit()
+
+# Init values
+I = 0
+LMSpeed = RMSpeed = 0
+lastError = 0
+
+# Main
 while True:
-# 1/4 speed
-  #LEFT
-   #front
-#    LMotorRedPin.low()
-    pwmLeftMotorR.duty_u16(16383)
-    time.sleep(2)
-   #back
-   # LMotorBlackPin.low()
-    pwmLeftMotorB.duty_u16(16383)
-    time.sleep(2)
-
-  #RIGHT
-   #front
-   # RMotorRedPin.low()
-    pwmRightMotorR.duty_u16(16383)
-    time.sleep(2)
-   #back
-  #  RMotorBlackPin.low()
-    pwmRightMotorB.duty_u16(16383)
-    time.sleep(2)
-
-
-#1/2 speed: #32767
-#full: #65535
-
-def RForward():
-    RMotorRedPin.value(1)
-    RMotorBlackPin.value(0)
+    error = IRPosition()
+    #print(f'Error: {error}')
+    time.sleep(0.1)
     
-def RBackward():
-    RMotorRedPin.value(0)
-    RMotorBlackPin.value(1)
+    P = error
+    I = I + error
+    D = error - lastError
+    lastError = error
 
-def RStop():
-    RMotorRedPin.value(0)
-    RMotorBlackPin.value(0)
-
-def LForward():
-    LMotorRedPin.value(1)
-    LMotorBlackPin.value(0)
+    motorSpeed = P*Kp + I*Ki + D*Kd
+    #print(f'PID: {motorSpeed}')
     
-def LBackward():
-    LMotorRedPin.value(0)
-    LMotorBlackPin.value(1)
+    LMSpeed_per = LMSpeed + motorSpeed
+    if LMSpeed_per > 100:
+        LMSpeed_per = 100
+    elif LMSpeed_per < 0:
+        LMSpeed_per = 0
+    #print(f'Speed Left: {LMSpeed_per}')
 
-def LStop():
-    LMotorRedPin.value(0)
-    LMotorBlackPin.value(0)
+    RMSpeed_per = RMSpeed - motorSpeed
+    if RMSpeed_per > 100:
+        RMSpeed_per = 100
+    elif RMSpeed_per < 0:
+        RMSpeed_per = 0
+    #print(f'Speed Right: {RMSpeed_per}')
 
-#while True:
-  #  RForward()
-   # LForward()
-    #time.sleep(0.3)
+    LMSpeed = LMSpeed_per
+    pwmLMRed.duty_u16(percentage(LMSpeed))
+    pwmLMBlack.duty_u16(0)
+    
+    RMSpeed = RMSpeed_per
+    pwmRMRed.duty_u16(percentage(RMSpeed))
+    pwmRMBlack.duty_u16(0)
 
-    #RStop()
-    #LStop()
-    #time.sleep(1)
-
-   # RBackward()
-   # LBackward()
-   # time.sleep(0.3)
-
-   # RStop()
-   # LStop()
-    #time.sleep(1)
